eead3334ee30c5de5c95aaf632519c221924f510
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-08-18 05:26:42 +0300

    ADD main scraper class tested and added

--- lib/MainScraper.rb
+++ lib/MainScraper.rb
@@ -1,22 +1,13 @@
-# frozen_string_literal: true
-
 require 'nokogiri'
 require 'open-uri'
 require 'json'
 
 class MainScraper
-  def initialize(url)
+  def initialize(url, league)
     @url = url
-    
-    parse_url
-  end
-
-  def parse_url
-    @parsed_url = Nokogiri::HTML(URI.open(@url))
-    @first = @parsed_url.at('tbody')
-    @table_rows = @first.css('tr')
-    @hash_1 = {}
-    h_array = %i[
+    @league = league
+    @team_hash = {}
+    @stat_type = %i[
       name
       Matches_Played
       Win
@@ -32,26 +23,29 @@ class MainScraper
       XGDiff_90
       url
     ]
-    # @first[0].css('td').length.times do |i|
+    parse_url
+  end
+
+  private
+
+  def parse_url
+    @parsed_url = Nokogiri::HTML(URI.open(@url))
+    @table_rows = @parsed_url.at('tbody').css('tr')
     @table_rows.length.times do |i|
-      @hash_1[@table_rows[i].css('td')[0].text] = {}
-      # @hash_1.merge!( h_array[0] => 'gfdgdf')
-      (h_array.length - 1).times do |j|
-        @hash_1[@table_rows[i].css('td')[0].text].merge!(h_array[j] => @table_rows[i].css('td')[j].text)
+      @team_hash[@table_rows[i].css('td')[0].text] = {}
+      (@stat_type.length - 1).times do |j|
+        @team_hash[@table_rows[i].css('td')[0].text].merge!(@stat_type[j] => @table_rows[i].css('td')[j].text)
       end
-      @hash_1[@table_rows[i].css('td')[0].text].merge!(h_array[-1] => @table_rows[i].css('td')[0].css('a').first['href'])
+      @team_hash[@table_rows[i].css('td')[0].text].merge!(@stat_type[-1] => @table_rows[i].css('td')[0].css('a').first['href'])
     end
-    p @hash_1 = @hash_1.sort_by { |club_name, _attrib| club_name }.to_h
-    File.write('./lib/sample.json', JSON.dump(@hash_1))
+    @team_hash = @team_hash.sort_by { |club_name, _attrib| club_name }.to_h
+    teams_to_json
+  end
+
+  def teams_to_json
+    File.write("./docs/#{@league}.json", JSON.dump(@team_hash))
   end
 end
 
-# file = File.read('./lib/sample.json')
-# data_hash1 = JSON.parse(file)
-# p data_hash1
-# File.write('./lib/sample.json', JSON.dump(hash1))
-
-dir = Dir.pwd
-# url3 = File.open("#{dir.to_s}/lib/test.html")
 url4 = 'https://fbref.com/en/comps/9/Premier-League-Stats'
-new_file = MainScraper.new(url4)
+new_file = MainScraper.new(url4, 'Premier League')
--- lib/PL.rb
+++ lib/PL.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 hash_1 = {}
 main = {}
 arr = %i[mm ll ee]
