d0565cd8beaa376b41fa9224ffe0cd432f1c6a96
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-08-19 00:14:07 +0300

    ADD array and hash impelementation tested and added

--- lib/player_scraper.rb
+++ lib/player_scraper.rb
@@ -22,12 +22,14 @@ class PlayerScraper < Parser
     G_plus_A_per_90mins
   ].freeze
 
-  SITE = 'https://fbref.com'
-  CLUB = 'CLUBS'
+  SITE = 'https://fbref.com'.freeze
+  CLUB = 'CLUBS'.freeze
   def initialize(league)
     @league = league
     @file_name = "#{league} Players"
     @url_array = {}
+    @players_outter_hash = {}
+    @players_inner_hash = {}
     add_clubs_url
   end
 
@@ -45,12 +47,18 @@ class PlayerScraper < Parser
     table_rows = parsed_url.at('tbody').css('tr')
     # p table_rows[33].css('th').text.strip
     # p table_rows[33].css('td > a > span')[0].text.gsub(/[^[:upper:]]+/, '').strip
-    table_rows.length.times do |j|
-      p table_rows[j].css('th').text.strip
-      PLAYER_STAT_TYPE.length.times do |i|
-        print "-- #{table_rows[j].css('td')[i].text.gsub(/[[:lower:]]+/, '').strip} --"
+    @players_outter_hash[@url_array.first.first] = { @players_inner_hash => {} }
+    table_rows.length.times do |i|
+      # p table_rows[i].css('th').text.strip
+      @players_inner_hash[table_rows[i].css('th').text.strip] = {}
+      PLAYER_STAT_TYPE.length.times do |j|
+        # print "-- #{table_rows[i].css('td')[i].text.gsub(/[[:lower:]]+/, '').strip} --"
+        # table_rows[i].css('th').text.strip.merge!(PLAYER_STAT_TYPE[i] => table_rows[i].css('td')[i].text.gsub(/[[:lower:]]+/, '').strip) 
+        @players_inner_hash[table_rows[i].css('th').text.strip].merge!( PLAYER_STAT_TYPE[j] => table_rows[i].css('td')[j].text.gsub(/[[:lower:]]+/, '').strip)
       end
+      @players_outter_hash[@url_array.first.first].merge!(@players_inner_hash)
     end
+    p @players_outter_hash
   end
 end
 
---
+++ lib/test.rb
@@ -0,0 +1,30 @@
+require 'json'
+hash1 ={first_person: {ber: 'ber', ket: 'ket'}}
+bash = {}
+# arr = JSON.parse(File.read("test.json"))
+# arr << hash
+# 
+filename = 'test.json'
+
+hash ={sec_person: { "first_name" => "john", "last_name" => "doe" }}
+
+# array = JSON.parse(File.read(filename))
+# array << hash
+
+# JSON.parse(json, :quirks_mode => true)
+# bash.merge!(hash1)
+# bash.merge!(hash)
+# p bash
+
+# File.write("test.json", JSON.dump(hash1))
+if File.read("test.json") != ''
+  file = JSON.parse(File.read("test.json"))
+  file.merge!(hash)
+  File.write("test.json", JSON.dump(file))
+else
+  File.write("test.json", JSON.dump(hash1))
+end
+
+p file = JSON.parse(File.read("test.json"))
+
+
